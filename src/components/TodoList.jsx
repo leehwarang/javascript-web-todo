2266ff30a5a2ebf13d39c2aa58883db8872014b5
leehwarang <hwrng2@gmail.com>
2019-09-20 16:48:56 +0900

    refactor: 라우터 처리 구조 변경을 위해 필요한 컴포넌트 추가
    - All, Todo, Done 컴포넌트 추가
    - 위 컴포넌트에 따라 다른 리스트를 만들기 위해 TodoList 컴포넌트 수정

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,27 +5,43 @@ import { IconButton } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import propTypes from "prop-types";
 
-const TodoList = ({ onChangeHandler, onDeleteHandler }) => {
+const TodoList = ({ onChangeHandler, onDeleteHandler, mode }) => {
   const { todoData, error, loading } = useContext(TodoContext);
 
-  const makeLiData = todos => {
-    return todos.map(todo => {
-      return (
-        <LI onClick={() => onChangeHandler(todo)} key={todo.id}>
-          {todo.status === "todo" ? todo.title : <del>{todo.title}</del>}
-          <IconButton id={todo.id} onClick={e => onDeleteHandler(e, todo)}>
-            <DeleteIcon fontSize="small" />
-          </IconButton>
-        </LI>
-      );
-    });
+  const makeLiData = (todos, mode) => {
+    console.log(todos, mode);
+    let datas = todos;
+    if (mode === "all") {
+      return [...todos].map(todo => {
+        return (
+          <LI onClick={() => onChangeHandler(todo)} key={todo.id}>
+            {todo.status === "todo" ? todo.title : <del>{todo.title}</del>}
+            <IconButton id={todo.id} onClick={e => onDeleteHandler(e, todo)}>
+              <DeleteIcon fontSize="small" />
+            </IconButton>
+          </LI>
+        );
+      });
+    } else {
+      datas = datas.filter(todo => todo.status === mode);
+      return datas.map(todo => {
+        return (
+          <LI onClick={() => onChangeHandler(todo)} key={todo.id}>
+            {todo.status === "todo" ? todo.title : <del>{todo.title}</del>}
+            <IconButton id={todo.id} onClick={e => onDeleteHandler(e, todo)}>
+              <DeleteIcon fontSize="small" />
+            </IconButton>
+          </LI>
+        );
+      });
+    }
   };
 
-  const makeLiComponent = todoData => {
+  const makeLiComponent = (todoData, mode) => {
     const todos = todoData;
     const isEmpty = !todos.length;
 
-    const result = isEmpty ? <li>없음</li> : makeLiData(todos);
+    const result = isEmpty ? <li>없음</li> : makeLiData(todos, mode);
     return result;
   };
 
@@ -33,7 +49,7 @@ const TodoList = ({ onChangeHandler, onDeleteHandler }) => {
     <>
       {error && <li>네트워크 요청 실패</li>}
       {loading && <li>로딩중...</li>}
-      {!error && !loading && makeLiComponent(todoData)}
+      {!error && !loading && makeLiComponent(todoData, mode)}
     </>
   );
 };
