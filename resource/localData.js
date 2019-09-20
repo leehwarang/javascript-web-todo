9eb2d89cc36c7c0c9943b53c3a21bbedd236543f
leehwarang <hwrng2@gmail.com>
2019-09-20 16:51:17 +0900

    add&refactor: 로컬 데이터 추가 및 변경된 bundle 파일 적용

--- resource/localData.js
+++ resource/localData.js
@@ -3,6 +3,7 @@ module.exports = {
     { title: "공부하기", id: 1233, status: "todo" },
     { title: "스터디준비", id: 1234, status: "todo" },
     { title: "알고리즘공부", id: 1230, status: "todo" },
-    { title: "컴퓨터게임", id: 1231, status: "todo" }
+    { title: "컴퓨터게임", id: 1231, status: "todo" },
+    { title: "강아지 산책", id: 1232, status: "done" }
   ]
 };
