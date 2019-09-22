bde9d0ecadf07cc61a12446ab88696d726f44843
leehwarang <hwrng2@gmail.com>
2019-09-22 23:14:08 +0900

    style: App 컴포넌트의 스타일을 scss로 적용
    - styled-component 제거
    - index.jsx에서도 base.scss를 불러와서 스타일 적용

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,20 @@
 import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
 import { ToDoStore } from "../../provider/ToDoStore";
 import AddTodo from "../AddTodo/AddTodo.jsx";
 import Counter from "../Counter/Counter.jsx";
 import NavTodo from "../NavTodo/NavTodo.jsx";
+import "./App.scss";
 
 const App = () => {
   return (
-    <>
-      <GlobalStyle />
+    <div className="App">
       <ToDoStore>
         <AddTodo />
         <Counter />
         <NavTodo />
       </ToDoStore>
-    </>
+    </div>
   );
 };
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #fffcf0;
-    max-width: 800px;
-    margin: 0 auto;
-    font-family: Monospace;
-  }
-`;
 
 export default App;
