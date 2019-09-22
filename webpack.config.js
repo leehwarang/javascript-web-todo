96b4dc010d72205fc2d4b3f0b2d6be9ce95ea678
leehwarang <hwrng2@gmail.com>
2019-09-23 00:01:23 +0900

    setting: webpack.dev에도 동일한 설정 추가
    - style-loader, scss-loader, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,21 @@ module.exports = {
           }
         },
         exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ],
+        exclude: /node_modules/
       }
     ]
   },
