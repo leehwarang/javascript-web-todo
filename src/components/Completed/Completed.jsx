862e1e27af9567dc2b74b6b840ce701d3541af50
leehwarang <hwrng2@gmail.com>
2019-09-24 15:55:31 +0900

    renamed: Todo -> Doing, Done -> Completed로 컴포넌트 이름 변경

--- src/components/Completed/Completed.jsx
+++ src/components/Completed/Completed.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
-import TodoList from "../TodoList/TodoList.jsx";
+import ContentList from "../ContentList/ContentList.jsx";
 import { TodoContext } from "../../provider/ToDoStore.jsx";
 
-export default function Done({ toggle }) {
-  console.log("Done");
-
+export default function Completed({ toggle }) {
   return (
     <div>
       <h2>‍👏 Congratulations! </h2>
       <div>
         <ul style={{ display: toggle ? "block" : "none" }}>
-          <TodoList mode="done" />
+          <ContentList mode="done" />
         </ul>
       </div>
     </div>
--- src/components/Doing/Doing.jsx
+++ src/components/Doing/Doing.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect, useContext } from "react";
-import TodoList from "../TodoList/TodoList.jsx";
+import ContentList from "../ContentList/ContentList.jsx";
 import { TodoContext } from "../../provider/ToDoStore";
 
-export default function Todo({ toggle }) {
-  console.log("Todo");
-
+export default function Doing({ toggle }) {
   return (
     <div>
       <h2>‍🚀 Cheer Up Bro!</h2>
 
       <div>
         <ul style={{ display: toggle ? "block" : "none" }}>
-          <TodoList mode="todo" />
+          <ContentList mode="todo" />
         </ul>
       </div>
     </div>
